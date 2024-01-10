2e6d12088144d314ce69fa8379faab91d51b8926
Hatcher Blair <hatcherbb@gmail.com>
2024-01-10 15:26:32 -0700

    Added reset Game and Session Buttons

--- script.js
+++ script.js
@@ -12,15 +12,22 @@ const game = (function () {
 
     // Setters
     const setPlayerScore = (player) => player == 'X' ? playerXScore++: playerOScore++;
+    const resetScores = () => {
+        playerXScore = 0;
+        playerOScore = 0;
+    }
+    const setPlayerTurn = (player) => playerXTurn = player;
     const setGameArray = (array) => gameArray = array;
     const setGameTile = (i, j) => {
         gameArray[i][j] = playerXTurn ? 'X' : 'O';
         playerXTurn = !playerXTurn;
         const isOver = gameOver();
+        if (isOver == 'X') setPlayerScore('X');
+        else if (isOver == 'O') setPlayerScore('O');
         if (isOver) gameBoard.displayGameOver(isOver);
     }
     
-    // Prints the gameboard in the console
+    // Prints the gameboard in the console (No longer used)
     const printGame = () => console.log(gameArray);
 
     // Check to see if game is over
@@ -28,97 +35,39 @@ const game = (function () {
         // Player X
         // Checking for Top row and Left Column
         if (gameArray[0][0] == 'X') {
-            if (gameArray[0][1] == 'X' && gameArray[0][2] == 'X') {
-                // Top row win
-                setPlayerScore('X');
-                return 'X';
-            }
-            if (gameArray[1][0] == 'X' && gameArray[2][0] == 'X') {
-                // Left Column win
-                setPlayerScore('X');
-                return 'X';
-            }
+            if (gameArray[0][1] == 'X' && gameArray[0][2] == 'X') return 'X'; // Top row win
+            if (gameArray[1][0] == 'X' && gameArray[2][0] == 'X') return 'X'; // Left Column win
         }
         // Checking for Bottom row and Right column
         if (gameArray[2][2] == 'X') {
-            if (gameArray[2][1] == 'X' && gameArray[2][0] == 'X') {
-                // Bottom row win
-                setPlayerScore('X');
-                return 'X';
-            }
-            if (gameArray[1][2] == 'X' && gameArray[0][2] == 'X') {
-                // Right column win
-                setPlayerScore('X');
-                return 'X';
-            }
+            if (gameArray[2][1] == 'X' && gameArray[2][0] == 'X') return 'X'; // Bottom row win
+            if (gameArray[1][2] == 'X' && gameArray[0][2] == 'X') return 'X'; // Right column win
         }
         // Checking for diagnols and middle row/column
         if (gameArray[1][1] == 'X' ) {
-            if (gameArray[1][0] == 'X' && gameArray[1][2] == 'X') {
-                // Middle row win
-                setPlayerScore('X');
-                return 'X';
-            } else if (gameArray[0][1] == 'X' && gameArray[2][1] == 'X') {
-                // Middle column win
-                setPlayerScore('X');
-                return 'X';
-            } else if (gameArray[0][0] == 'X' && gameArray[2][2] == 'X') {
-                // Left diagnol win
-                setPlayerScore('X');
-                return 'X';
-            } else if (gameArray[2][0] == 'X' && gameArray[0][2] == 'X') {
-                // Right diagnol win
-                setPlayerScore('X');
-                return 'X';
-            }
+            if (gameArray[1][0] == 'X' && gameArray[1][2] == 'X')       return 'X'; // Middle row win
+             else if (gameArray[0][1] == 'X' && gameArray[2][1] == 'X') return 'X'; // Middle column win
+             else if (gameArray[0][0] == 'X' && gameArray[2][2] == 'X') return 'X'; // Left diagnol win
+             else if (gameArray[2][0] == 'X' && gameArray[0][2] == 'X') return 'X'; // Right diagnol win
         }
 
         // Player O
         // Checking for Top row and Left Column
         if (gameArray[0][0] == 'O') {
-            if (gameArray[0][1] == 'O' && gameArray[0][2] == 'O') {
-                // Top row win
-                setPlayerScore('O');
-                return 'O';
-            }
-            if (gameArray[1][0] == 'O' && gameArray[2][0] == 'O') {
-                // Left Column win
-                setPlayerScore('O');
-                return 'O';
-            }
+            if (gameArray[0][1] == 'O' && gameArray[0][2] == 'O') return 'O'; // Top row win
+            if (gameArray[1][0] == 'O' && gameArray[2][0] == 'O') return 'O'; // Left Column win
         } 
         // Checking for Bottom row and Right column
         if (gameArray[2][2] == 'O') {
-            if (gameArray[2][1] == 'O' && gameArray[2][0] == 'O') {
-                // Bottom row win
-                setPlayerScore('O');
-                return 'O';
-            }
-            if (gameArray[1][2] == 'O' && gameArray[0][2] == 'O') {
-                // Right column win
-                setPlayerScore('O');
-                return 'O';
-            }
+            if (gameArray[2][1] == 'O' && gameArray[2][0] == 'O') return 'O'; // Bottom row win
+            if (gameArray[1][2] == 'O' && gameArray[0][2] == 'O') return 'O'; // Right column win
         }
         // Checking for diagnols and middle row/column
         if (gameArray[1][1] == 'O' ) {
-            if (gameArray[1][0] == 'O' && gameArray[1][2] == 'O') {
-                // Middle row win
-                setPlayerScore('O');
-                return 'O';
-            } else if (gameArray[0][1] == 'O' && gameArray[2][1] == 'O') {
-                // Middle column win
-                setPlayerScore('O');
-                return 'O';
-            } else if (gameArray[0][0] == 'O' && gameArray[2][2] == 'O') {
-                // Left diagnol win
-                setPlayerScore('O');
-                return 'O';
-            } else if (gameArray[2][0] == 'O' && gameArray[0][2] == 'O') {
-                // Right diagnol win
-                setPlayerScore('O');
-                return 'O';
-            }
+            if (gameArray[1][0] == 'O' && gameArray[1][2] == 'O')       return 'O'; // Middle row win
+             else if (gameArray[0][1] == 'O' && gameArray[2][1] == 'O') return 'O'; // Middle column win
+             else if (gameArray[0][0] == 'O' && gameArray[2][2] == 'O') return 'O'; // Left diagnol win
+             else if (gameArray[2][0] == 'O' && gameArray[0][2] == 'O') return 'O'; // Right diagnol win
         }
 
         // Check for draw
@@ -135,11 +84,37 @@ const game = (function () {
         return false;
     };
 
-    return { getPlayerScore, getGameArray, setPlayerScore, printGame, gameOver, setGameArray, setGameTile};
+    return { getPlayerScore, getGameArray, setPlayerScore, gameOver, 
+            setGameArray, setGameTile, setPlayerTurn, resetScores};
 })();
 
 // Manages the visuals for the game
 const gameBoard = (function ()  {
+
+    // Score display DOM elements
+    const playerXScoreDOM = document.getElementById("player-x");
+    const playerOScoreDOM = document.getElementById("player-o");
+    const scoreDOMLength = 16;
+
+    // Reset game eventListener
+    const resetGameBtn = document.querySelector('.reset-game-btn');
+    resetGameBtn.addEventListener('click', () => {
+        newBoard();
+        // True for X, false for O
+        // Always setting X as first player
+        game.setPlayerTurn(true);
+    });
+
+    // Reset session eventListener
+    const resetSessionBtn = document.querySelector('.reset-session-btn');
+    resetSessionBtn.addEventListener('click', () => {
+        newBoard();
+        game.resetScores();
+        game.setPlayerTurn(true);
+        playerXScoreDOM.textContent = 'Player X Score: 0';
+        playerOScoreDOM.textContent = 'Player O Score: 0';
+    })
+
     // Creates the board visual
     const newBoard = () => {
         game.setGameArray([[], [], []]);
@@ -197,8 +172,14 @@ const gameBoard = (function ()  {
             modalTextBox.textContent = 'Draw! Nobody wins :(';
         } else if (winner == 'X') {
              modalTextBox.textContent += 'X!';
+             // Update scoreboard visual here so we don't need to check winner twice
+             playerXScoreDOM.textContent = playerXScoreDOM.textContent.slice(0, scoreDOMLength) 
+                                            + game.getPlayerScore('X');
         } else {
             modalTextBox.textContent += 'O!';
+            // Update scoreboard visual here so we don't need to check winner twice
+            playerOScoreDOM.textContent = playerOScoreDOM.textContent.slice(0, scoreDOMLength)
+                                            + game.getPlayerScore('O');
         }        
         gameOverBody.appendChild(modalTextBox);
 
